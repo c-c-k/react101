f7c68d86b188aca06d93879dd2a56af9cc49f204
c-c-k <c-c-k@nym.hush.com>
2023-06-07 11:10:25 +0300

    Reformatted and added lesson choice to App

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,13 +1,34 @@
 // import logo from './logo.svg';
-import './App.css';
-import GradesTable from './lessons/1/components/GradesTable.jsx'
+import { Component } from "react";
+import "./App.css";
+import GradesTable from "./lessons/1/components/GradesTable.jsx";
+import ClickMe from "./lessons/2/components/ClickMe.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <GradesTable />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { main_component: "" };
+  }
+  setLesson1 = () => this.setState({ main_component: <GradesTable /> });
+  setLesson2 = () => this.setState({ main_component: <ClickMe /> });
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <h2> nav </h2> <hr />
+          <button type="submit" onClick={this.setLesson1}>
+            Lesson 1: grades
+          </button>
+          <button type="submit" onClick={this.setLesson2}>
+            Lesson 2: Click Me
+          </button>
+        </nav>
+        <main autoFocus="true" role="main">
+          {this.state.main_component}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
