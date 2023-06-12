d2ac04d6afe1685fb130306e325d5ae582af745a
c-c-k <c-c-k@nym.hush.com>
2023-06-12 12:32:01 +0300

    main page style fix

--- app2/src/app/page.js
+++ app2/src/app/page.js
@@ -1,32 +1,37 @@
 "use client";
 import "./page.css";
-import {useState} from "react";
+import { useState } from "react";
 import GradesTable from "./lessons/1/components/GradesTable.jsx";
 import ClickMe from "./lessons/2/components/ClickMe.jsx";
 import KeyChange from "./lessons/3/components/KeyChange.jsx";
+import TableWithForm from "./lessons/4/components/TableWithForm.jsx";
 
 export default function Home() {
-  const [lesson, setLesson] = useState(<KeyChange />);
+  const [lesson, setLesson] = useState(<TableWithForm />);
   const setLesson1 = () => setLesson(<GradesTable />);
   const setLesson2 = () => setLesson(<ClickMe />);
   const setLesson3 = () => setLesson(<KeyChange />);
+  const setLesson4 = () => setLesson(<TableWithForm />);
   return (
     <div className="App">
-        <nav>
-          <h2> nav </h2> <hr />
-          <button type="submit" onClick={setLesson1}>
-            Lesson 1: grades
-          </button>
-          <button type="submit" onClick={setLesson2}>
-            Lesson 2: Click Me
-          </button>
-          <button type="submit" onClick={setLesson3}>
-            Lesson 3: Key Change
-          </button>
-        </nav>
-        <main autoFocus={true} role="main">
-          {lesson}
-        </main>
+      <nav>
+        <h2> nav </h2> <hr />
+        <button type="submit" onClick={setLesson1}>
+          <p>Lesson 1:</p> <p>grades</p>
+        </button>
+        <button type="submit" onClick={setLesson2}>
+          <p>Lesson 2:</p> <p>Click Me</p>
+        </button>
+        <button type="submit" onClick={setLesson3}>
+          <p>Lesson 3:</p> <p>Key Change</p>
+        </button>
+        <button type="submit" onClick={setLesson4}>
+          <p>Lesson 4:</p> <p>Table With Form</p>
+        </button>
+      </nav>
+      <main autoFocus={true} role="main">
+        {lesson}
+      </main>
     </div>
   );
 }
